37581bcca75f244c260d0f963cff1d8198d21763
Kartikey Pandey <kartikey224252@gmail.com>
2024-11-29 01:05:42 +0530

    added some changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    extensions: [".js", ".ts", ".jsx", ".tsx"], // Ensure these extensions are correctly resolved
+  },
+  server: {
+    mimeTypes: {
+      "application/javascript": ["js", "mjs"],
+    },
+    cors: true,
+  },
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
 });
